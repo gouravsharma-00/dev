55ddcf1dbe414150f5d7dd547f5cd840d85da118
Gourav sharma <gourav.sharma.glamat2932@gmail.com>
2025-02-14 17:49:54 +0530

    initial

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function RootLayout({children}) {
+    return(
+        <html>
+            <head>
+                
+            </head>
+            <body>
+                
+            </body>
+        </html>
+    )
+}
